4fda35a43d51a62b63c3058d7331e8280f794922
ws-kudryavets <46526630+ws-kudryavets@users.noreply.github.com>
2021-04-17 02:57:00 +0300

    Changes

--- src/components/Hack.js
+++ src/components/Hack.js
@@ -3,11 +3,23 @@ import atomize from "@quarkly/atomize";
 
 const Hack = props => {
 	useEffect(() => {
-		// var data = [{type: 'densitymapbox', lon: [10, 20, 30], lat: [15, 25, 35], z: [1, 3, 2]}];
-		// var layout = {width: 600, height: 400, mapbox: {style: 'stamen-terrain'}};
-		// document.querySelector('#myDiv') && Plotly.newPlot('myDiv', data, layout);
+		var data = [{
+			type: 'densitymapbox',
+			lon: [10, 20, 30],
+			lat: [15, 25, 35],
+			z: [1, 3, 2]
+		}];
+		var layout = {
+			width: 600,
+			height: 400,
+			mapbox: {
+				style: 'stamen-terrain'
+			}
+		};
+		document.querySelector('#myDiv') && Plotly.newPlot('myDiv', data, layout);
+
 		if (document.querySelector('a[href="https://quarkly.io/"')) {
-			document.querySelector('a[href="https://quarkly.io/"').remove();
+			document.querySelector('a[href="https://quarkly.io/"').hidden = true;
 		}
 	}, []);
 	return <div {...props}>
