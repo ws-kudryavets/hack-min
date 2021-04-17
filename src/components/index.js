72064882bec4a86a3ee3d0f9c92f6a52b701fa1d
ws-kudryavets <46526630+ws-kudryavets@users.noreply.github.com>
2021-04-17 03:02:41 +0300

    Changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Hack } from "./Hack"
 export { default as LoginForm } from "./LoginForm"
 export { default as Footer } from "./Footer"
+export { default as EmbedJS } from "./EmbedJS"
 export { default as ChartPage } from "./ChartPage"
