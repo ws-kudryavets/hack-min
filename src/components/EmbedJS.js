540b5b0591efdd4a24b2ebb9b57edb8da66f9768
ws-kudryavets <46526630+ws-kudryavets@users.noreply.github.com>
2021-04-17 23:08:21 +0300

    Changes

--- src/components/EmbedJS.js
+++ src/components/EmbedJS.js
@@ -1,9 +1,9 @@
 import React, { useRef, useLayoutEffect } from "react";
 import atomize from "@quarkly/atomize";
 import Data from './Data';
-import { ege } from './ChartData'; // Put your JS here:
+import { ege, dv, pr, sz, si, sk, ur, uj } from './ChartData'; // Put your JS here:
 
-const customJs = `${Data}${ege}
+const customJs = `${Data}${ege}${dv}${pr}${sz}${si}${sk}${ur}${uj}
 if (document.getElementById("5ea125cc-21f7-401e-adfb-003960b4c4f0")) {
                     Plotly.newPlot(
                         '5ea125cc-21f7-401e-adfb-003960b4c4f0',
